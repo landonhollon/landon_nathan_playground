27c2fabef32555475206d7e33cae1e27f64cf5ea
Nathan Russell <nathan@blackpathway.com>
2024-02-28 22:02:18 -0500

    new stuff for landon to do

--- hello.py
+++
@@ -1,4 +0,0 @@
-while True:
-    a = int(input("Give me a number "))
-    b = int(input("Give me a number "))
-    print("The sum of the two numbers is ",a+b)
---
+++ sayHello.py
@@ -0,0 +1,1 @@
+print("Hello John!  How are you?")
